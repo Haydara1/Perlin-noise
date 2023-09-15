61467147ebb3939b8b95084044e3984cd311d9d8
Haydara <haydaraal2005@gmail.com>
2023-09-15 10:44:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Perlin_noise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
